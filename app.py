270c1c72e0a8e4a0bbb98beaf551bc6fcda60aad
josemiiguel12 <108009248+josemiiguel12@users.noreply.github.com>
2022-06-22 10:48:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #tabla filtrada con información nutricionak
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show )
