b5c62bd73cc1e8a42b25fb61232be9dfea1dbab6
josemiiguel12 <108009248+josemiiguel12@users.noreply.github.com>
2022-06-22 10:39:49 -0400

    Update app.py
    carga de datos csv

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas 
 
 streamlit.title('Esto es un titulo')
 
@@ -8,3 +9,8 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
 
+#Agregar una sección para crear tu batido
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
