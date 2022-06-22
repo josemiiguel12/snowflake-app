61eafe5708475f038c3c4ed631d57208458eeca4
josemiiguel12 <108009248+josemiiguel12@users.noreply.github.com>
2022-06-22 10:47:59 -0400

    Update app.py
    Creación de selección múltiple y filtrado

--- app.py
+++ app.py
@@ -12,5 +12,17 @@ streamlit.text('🥑🍞 Avocado toast')
 #Agregar una sección para crear tu batido
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#cargar un listado a un dataframe desde s3
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#crear un filtrado con multiples selecciones 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Seleccion multiple de frutas
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+#filtro a nivel de tabla
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#tabla filtrada con información nutricionak
 streamlit.dataframe(my_fruit_list)
