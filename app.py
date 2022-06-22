09b5880c3b2b65743456a859eb01cf97ba494aae
josemiiguel12 <108009248+josemiiguel12@users.noreply.github.com>
2022-06-22 10:34:48 -0400

    Update app.py
    Se agrego lista

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('Esto es un titulo')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+
